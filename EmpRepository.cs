cd359f70da63e66520762e49c08f1428624ca684
kartheek88 <karthikeya08818@gmail.com>
2024-09-04 21:21:59 +0530

    EmployeeAPI

--- EmpRepository.cs
+++ EmpRepository.cs
@@ -13,38 +13,7 @@ namespace EmployeeAPI
         }
 
 
-        public async Task AddEmployeeAsync(EmpModel empModel)
-        {
-            _empcontext.Add(empModel);
-             await _empcontext.SaveChangesAsync();
-        }
-
-
-        public async Task<IEnumerable<EmpModel>> GelAllEmpAsync()
-        {
-            return await _empcontext.EmployeeList.ToListAsync();
-        }
-
-        public async Task <EmpModel> GetEmpByIdAsync( int id)
-        {
-            return await _empcontext.EmployeeList.FindAsync(id);
-        }
-
-        public async Task DeleteEmpAsync(EmpModel empModel,int id)
-        {
-            var Emp = await _empcontext.EmployeeList.FindAsync(id);
-            if (Emp != null)
-            {
-                _empcontext.EmployeeList.Remove(Emp);
-                await _empcontext.SaveChangesAsync();
-            }
-        }
-
-        public async Task UpdateEmpAsync(EmpModel empModel)
-        {
-            _empcontext.Entry(empModel).State = EntityState.Modified;
-            await _empcontext.SaveChangesAsync();
-        }
+       
 
     }
 }
--- IRepositories/IEmpRepo.cs
+++ IRepositories/IEmpRepo.cs
@@ -6,10 +6,6 @@ namespace EmployeeAPI.Repositories
 {
     public interface IEmpRepo
     {
-        Task AddEmployeeAsync(EmpModel empModel);
-        Task<IEnumerable<EmpModel>> GelAllEmpAsync();
-        Task<EmpModel> GetEmpByIdAsync(int id);
-        Task DeleteEmpAsync(EmpModel empModel, int id);
-        Task UpdateEmpAsync(EmpModel empModel);
+
     }
 }
